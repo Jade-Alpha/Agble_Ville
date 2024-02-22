972dbc650e1d3e37859daf466a74666b6ea4a919
Apoerakay <apoerak@gmail.com>
2024-02-22 08:39:57 -0800

    worked on team and footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import Team from "./Components/landing-page/footer-section";
 
 
 
+function App(){
+    return(
+        <>
+        <Team/>
+        </>
+    )
+}
+
+export default App;
--- src/Components/landing-page/footer-section/index.js
+++ src/Components/landing-page/footer-section/index.js
@@ -1,7 +1,32 @@
-function Footer(){
-    return
+import styles from "./index.module.css"
 
-    
-};
 
-export default Footer;
+function Team () {
+    return(
+
+        <>
+        <section className="section-white">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2 className={styles.TeamH2}>The team behind Agble Ville</h2>
+                        <p className={styles.TeamP}>meet the hard-working team that put this all together</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <footer>
+
+        </footer>
+        </>
+    )
+}
+        
+
+                
+export default Team;
