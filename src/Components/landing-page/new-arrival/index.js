8e6f0f9de45b23b216045abb8e65b1083adaa7de
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-25 01:33:41 +0000

    Update index.js

--- src/Components/landing-page/new-arrival/index.js
+++ src/Components/landing-page/new-arrival/index.js
@@ -1,6 +1,5 @@
-// NewArrival.js
 import React from "react";
-import styles from "./index.module.css"; // Import CSS module
+import styles from "./index.module.css";
 
 function NewArrival() {
     return (
@@ -16,39 +15,39 @@ function NewArrival() {
             <div className={styles.productCard}>
                 <img src="potatoes.avif" alt="" />
                 <div className={styles.productName}>Potatoes</div>
-                <div className={styles.productPrice}>$6</div>
+                <div className={styles.productPrice}><span>&#8373;</span>60</div>
                 <button className={styles.viewMoreButton}>View More</button>
             </div>
             <div className={styles.productCard}>
                 <img src="mushrooms.avif" alt="" />
                 <div className={styles.productName}>Mushrooms</div>
-                <div className={styles.productPrice}>$15</div>
+                <div className={styles.productPrice}><span>&#8373;</span>30</div>
                 <button className={styles.viewMoreButton}>View More</button>
             </div>
             <div className={styles.productCard}>
                 <img src="tomatoes.avif" alt="" />
                 <div className={styles.productName}>Tomatoes</div>
-                <div className={styles.productPrice}>$1</div>
+                <div className={styles.productPrice}><span>&#8373;</span>50</div>
                 <button className={styles.viewMoreButton}>View More</button>
             </div>
         
             <div className={styles.productCard}>
                 <img src="milk.avif" alt="" />
                 <div className={styles.productName}>Milk</div>
-                <div className={styles.productPrice}>$20</div>
+                <div className={styles.productPrice}><span>&#8373;</span>20</div>
                 <button className={styles.viewMoreButton}>View More</button>
             </div>
            
             <div className={styles.productCard}>
                 <img src="apples.avif" alt="" />
                 <div className={styles.productName}>Apples</div>
-                <div className={styles.productPrice}>$120,000</div>
+                <div className={styles.productPrice}><span>&#8373;</span>40</div>
                 <button className={styles.viewMoreButton}>View More</button>
             </div>
             <div className={styles.productCard}>
                 <img src="cheese.avif" alt="" />
                 <div className={styles.productName}>Cheese</div>
-                <div className={styles.productPrice}>$90</div>
+                <div className={styles.productPrice}><span>&#8373;</span>90</div>
                 <button className={styles.viewMoreButton}>View More</button>
             </div>
         </section>
