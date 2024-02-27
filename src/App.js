9451f0337feeeff484ac18591cbb62327fc042c0
mercykorkoressel <mercyessel1005@gmail.com>
2024-02-27 16:12:02 +0000

    mercy commt

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+// App.js
+import './App.css'
+// import React from 'react';
+import AboutSection from "../src/Components/landing-page/about-section"
+import SectionServiceItem from "../src/Components/landing-page/USP-section"
 
+
+function App () {
+  return (
+    <>
+      <AboutSection />
+      {/* Add other components and sections as needed */}
+    </>
+  );
+}
+
+export default App;
 
 
