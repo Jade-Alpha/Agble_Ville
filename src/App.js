5f84d0cc35e509eab476f64539c39b27eb5edbe0
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-28 13:53:41 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-// App.js
-import './App.css'
-// import React from 'react';
-import AboutSection from "../src/Components/landing-page/about-section"
-import SectionServiceItem from "../src/Components/landing-page/USP-section"
 import HomePage from "./pages/homePage"
 import React from "react";
 import NewArrival from "./Components/landing-page/new-arrival";
-// import Hero from "../src/Components/landing-page/hero-section"
+import Hero from "../src/Components/landing-page/hero-section"
+import AboutSection from "../src/Components/landing-page/about-section"
+import BuyerCatalogue from "./Components/BuyerCatalogue/BuyerCatalogue"
 
+
+
+export default App;
 
 function App() {
     return (
         <>
         <HomePage/>
-        {/* <Hero/> */}
-        <AboutSection />
+        <Hero/>
+         <AboutSection />
        <NewArrival />
+       <BuyerCatalogue/>
         </>
     )
 }
-
-
-
-
-export default App;
