060fac1ef7629c095690f58f912c9d4c49ccc4c6
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-28 16:57:00 +0000

    adding new features

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import NewArrival from "./Components/landing-page/new-arrival";
+import Hero from "../src/Components/landing-page/hero-section";
 import BuyerCatalogue from "./Components/BuyerCatalogue/BuyerCatalogue";
-import Cartpage from "./Components/CartBasket/Cartpage";
-import CheckoutPage from "./Components/CheckoutBasket/CheckoutPage";
+import AddItemForm from "./Components/SellerManagement/AddItemForm";
+import About from "./Components/landing-page/about-section/index";
+import EditItemForm from "./Components/SellerManagement/EditItemForm";
+import ItemList from "./Components/SellerManagement/ItemList";
+import Cart from "./Components/CartBasket/Cart";
+import CartItem from "./Components/CartBasket/CartItem";
+import CartPage from "./Components/CartBasket/Cartpage";
+import CheckoutForm from "./Components/CheckoutBasket/CheckoutForm";
+import CheckoutPage from "./Components/CheckoutBasket/CheckoutForm"
 
+export default App;
 
 function App() {
     return (
-        <Router>
-            <Routes>
-            <Route path="/" element={<BuyerCatalogue />} /> 
-                <Route path="/products" element={<BuyerCatalogue />} />
-                <Route path="/cart" element={<Cartpage />} /> 
-                <Route path="/checkout" element={<CheckoutPage />} /> 
-            </Routes>
-        </Router>
-    );
+        <>
+        <Hero/>
+        <About/>
+       <NewArrival />
+       <BuyerCatalogue/>
+       <Cart/>
+       <CartItem/>
+       <CartPage/>
+       <CheckoutForm/>
+       <CheckoutPage/>
+       <AddItemForm/>
+       <EditItemForm/>
+       <ItemList/>
+        </>
+    )
 }
-
-export default App;
