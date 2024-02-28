76a017a212261765e653a61dffcca2505ea7d6e6
Apoerakay <apoerak@gmail.com>
2024-02-28 09:33:45 -0800

    login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import SignUp_Page from "./Components/Sign_up/signup";
+import SignUp_Page from "./pages/Signup_page/signup.jsx";
 import Footer from "./Components/landing-page/footer-section";
-import Login_Page from "./Components/Login/login";
+import Login_Page from "./pages/login_page/login.jsx";
+import HomePage from "./pages/homePage"
+import React from "react";
 
 export default function App() {
-    return(
+    return (
         <>
-        { <SignUp_Page/> }
-        <HomePage/>
-        {/* { <Login_Page/> } */}
+            <HomePage />
+            <SignUp_Page />
+            <Login_Page />
         </>
     )
 }
-import HomePage from "./pages/homePage"
-import React from "react";
+
+
 
 
 
